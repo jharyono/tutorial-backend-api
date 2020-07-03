97da2c9fb05de5f0c9edede71b323ddafbd74ee2
Joni Haryono <jharyono@gmail.com>
2020-07-03 20:00:10 +0700

    Remove some unecessary code

--- app/models/index.js
+++ app/models/index.js
@@ -1,18 +1,25 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+// This configuration for production
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
 });
+
+// This configuration for development
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
+//   operatorsAliases: false,
+
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle
+//   }
+// });
 
 const db = {};
 
