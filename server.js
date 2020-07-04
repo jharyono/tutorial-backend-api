2e6c5d4b6a6fd8229e34f588c600108d03a83bf9
Joni Haryono <jharyono@gmail.com>
2020-07-04 08:54:29 +0700

    some changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://tutorials-api.herokuapp.com",
 };
 
 app.use(cors(corsOptions));
